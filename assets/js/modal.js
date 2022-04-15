4ef66ce822272eeec425d4a67f90c5d1cf8abf09
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-04-15 23:43:41 +0300

    feat: Add modal animations

---
+++ assets/js/modal.js
@@ -0,0 +1,7 @@
+$('#modal-container').removeAttr('class').addClass('one');
+$('body').addClass('modal-active');
+
+$('#modal-container').click(function() {
+    $(this).addClass('out');
+    $('body').removeClass('modal-active');
+});
