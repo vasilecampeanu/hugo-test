01d9a76dcfb16faf1d04784ccfc59cca741967ea
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-04-16 13:51:05 +0300

    feat: Working on article page

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,5 +1,5 @@
-$('#modal-container').removeAttr('class').addClass('one');
-$('body').addClass('modal-active');
+// $('#modal-container').removeAttr('class').addClass('one');
+// $('body').addClass('modal-active');
 
 $('#modal-container').click(function() {
     $(this).addClass('out');
